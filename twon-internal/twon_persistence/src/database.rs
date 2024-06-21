c8019e058f05b10ce868ea65b23055c862ad6337
Jeshua Hinostroza <hinostrozajeshua@gmail.com>
2024-06-20 21:29:29 -0500

    build: can connect to db in debug mode In order to speed up compilation times we can connect to the db rather than initializating it

--- twon-internal/twon_persistence/src/database.rs
+++ twon-internal/twon_persistence/src/database.rs
@@ -1,10 +1,13 @@
-use crate::create_local_path;
-use surrealdb::engine::local::Db;
 pub use surrealdb::Result;
 
-pub type Connection = surrealdb::Surreal<Db>;
+#[cfg(feature = "file")]
+pub type Connection  = surrealdb::Surreal<surrealdb::engine::local::Db>;
+#[cfg(feature = "remote")]
+pub type Connection = surrealdb::Surreal<surrealdb::engine::remote::ws::Client>;
+
 pub type Error = surrealdb::Error;
 
+#[cfg(feature = "file")]
 const DB_DIR: &str = "twon.db";
 
 async fn init(connection: &Connection) -> Result<()> {
@@ -32,8 +35,11 @@ async fn init(connection: &Connection) -> Result<()> {
     Ok(())
 }
 
-pub async fn connect() -> surrealdb::Result<Connection> {
-    let path = create_local_path().join(DB_DIR);
+#[cfg(feature = "file")]
+async fn create_connection() -> surrealdb::Result<(Connection, bool)> {
+    use surrealdb::engine::local::Db;
+
+    let path = crate::create_local_path().join(DB_DIR);
     let exists = tokio::fs::try_exists(&path).await.unwrap_or_else(|_| {
         println!("WARNING: Failed to check if db exists");
         false
@@ -44,6 +50,18 @@ pub async fn connect() -> surrealdb::Result<Connection> {
         path.display()
     ))
     .await?;
+
+    (db, exists)
+}
+
+#[cfg(feature = "remote")]
+async fn create_connection() -> surrealdb::Result<(Connection, bool)> {
+    let db: Connection = surrealdb::Surreal::new::<surrealdb::engine::remote::ws::Ws>("0.0.0.0:6767").await?;
+    Ok((db, false))
+}
+
+pub async fn connect() -> surrealdb::Result<Connection> {
+    let (db, exists) = create_connection().await?;
     db.use_ns("twon").use_db("twon").await?;
 
     if !exists {
