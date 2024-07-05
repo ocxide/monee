2558ce87cc8f70411e7d429975d738cd4c49d5fc
Jeshua Hinostroza <hinostrozajeshua@gmail.com>
2024-07-04 21:36:59 -0500

    chore(cli): open db file from git root

--- monee-cli/open-db.sh
+++ monee-cli/open-db.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-surreal start --bind 0.0.0.0:6767 file:./data/monee/monee.db
+REPO_ROOT="$(git rev-parse --show-toplevel)"
+
+surreal start --bind 0.0.0.0:6767 "file:$REPO_ROOT/monee-cli/data/monee/monee.db"
