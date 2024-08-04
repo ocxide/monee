7e2b007a8e1f6c5ae9b8c7918884c381d7d55228
Jeshua Hinostroza <hinostrozajeshua@gmail.com>
2024-08-04 12:00:26 -0500

    feat: can setup context

--- monee/src/lib.rs
+++ monee/src/lib.rs
@@ -45,7 +45,7 @@ pub mod backoffice {
 
                 use crate::{
                     backoffice::wallets::domain::{repository::Repository, wallet::Wallet},
-                    shared::{context::AppContext, errors::InfrastructureError},
+                    shared::{domain::context::AppContext, errors::InfrastructureError},
                 };
 
                 pub struct CreateOne {
@@ -62,9 +62,7 @@ pub mod backoffice {
 
                 impl CreateOne {
                     pub async fn run(&self, wallet: Wallet) -> Result<(), InfrastructureError> {
-                        self.repository
-                            .save(wallet)
-                            .await?;
+                        self.repository.save(wallet).await?;
 
                         Ok(())
                     }
@@ -103,7 +101,7 @@ pub mod backoffice {
 
                 use crate::{
                     backoffice::events::domain::{event::Buy, repository::Repository},
-                    shared::{context::AppContext, errors::InfrastructureError},
+                    shared::{domain::context::AppContext, errors::InfrastructureError},
                 };
 
                 pub struct AddBuy {
@@ -212,6 +210,15 @@ pub mod shared {
         #[error("infrastructure error: {0}")]
         pub struct InfrastructureError(Box<dyn std::error::Error>);
 
+        impl InfrastructureError {
+            pub fn new<E>(error: E) -> Self
+            where
+                E: Into<Box<dyn std::error::Error>>,
+            {
+                Self(error.into())
+            }
+        }
+
         #[derive(Debug, thiserror::Error)]
         pub enum SnapshotOptError {
             #[error(transparent)]
@@ -272,15 +279,89 @@ pub mod shared {
         }
     }
 
-    pub mod context {
-        pub trait AppContext {
-            fn backoffice_events_repository(
-                &self,
-            ) -> Box<dyn crate::backoffice::events::domain::repository::Repository>;
+    pub mod application {
+        pub mod cannonical_context {
+            use cream::{context::Context, cream_context::CreamContext};
 
-            fn backoffice_wallets_repository(
-                &self,
-            ) -> Box<dyn crate::backoffice::wallets::domain::repository::Repository>;
+            use crate::{
+                connect,
+                shared::{domain::context::AppContext, errors::InfrastructureError},
+            };
+
+            #[derive(Clone)]
+            pub struct CannocalContext {
+                cream_context: CreamContext,
+                database: crate::database::Connection,
+            }
+
+            #[derive(Debug, thiserror::Error)]
+            pub enum Error {
+                #[error(transparent)]
+                Infrastructure(#[from] InfrastructureError),
+            }
+
+            pub async fn setup(
+            ) -> Result<(CannocalContext, impl std::future::Future<Output = ()>), Error>
+            {
+                let db = connect().await.map_err(InfrastructureError::new)?;
+
+                let router = cream::event_router::EventRouter::default();
+                // Add event handlers
+
+                let (port, socket) = cream::event_bus::create_channel();
+
+                let ctx = CannocalContext {
+                    cream_context: CreamContext::new(port),
+                    database: db,
+                };
+
+                let listen = {
+                    let ctx = ctx.clone();
+                    async move {
+                        cream::event_bus::EventBus::new(socket, ctx, router)
+                            .listen()
+                            .await;
+                    }
+                };
+
+                Ok((ctx, listen))
+            }
+
+            impl Context for CannocalContext {}
+
+            impl AppContext for CannocalContext {
+                fn backoffice_events_repository(
+                    &self,
+                ) -> Box<dyn crate::backoffice::events::domain::repository::Repository>
+                {
+                    todo!()
+                }
+
+                fn backoffice_wallets_repository(
+                    &self,
+                ) -> Box<dyn crate::backoffice::wallets::domain::repository::Repository>
+                {
+                    todo!()
+                }
+            }
         }
     }
+
+    pub mod domain {
+        pub mod context {
+            use cream::context::Context;
+
+            pub trait AppContext: Context {
+                fn backoffice_events_repository(
+                    &self,
+                ) -> Box<dyn crate::backoffice::events::domain::repository::Repository>;
+
+                fn backoffice_wallets_repository(
+                    &self,
+                ) -> Box<dyn crate::backoffice::wallets::domain::repository::Repository>;
+            }
+        }
+    }
+
+    pub mod infrastructure {}
 }
