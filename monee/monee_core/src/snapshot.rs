cd2c690ee5cf56e1d73fd7f60a8115344d5b58b4
Jeshua Hinostroza <hinostrozajeshua@gmail.com>
2024-08-18 14:29:58 -0500

    feat(core): can have more data in MoneyMap

---
+++ monee/monee_core/src/snapshot.rs
@@ -0,0 +1,214 @@
+use money::{MoneyError, MoneyHost};
+
+use crate::{ActorId, Amount, CurrencyId, DebtId, WalletId};
+
+pub use money::{Money, MoneyMap};
+
+mod money;
+
+#[derive(Debug, Clone, serde::Serialize, serde::Deserialize)]
+pub struct Wallet(Money);
+
+impl AsMut<Money> for Wallet {
+    fn as_mut(&mut self) -> &mut Money {
+        &mut self.0
+    }
+}
+
+impl MoneyHost for Wallet {
+    type Key = WalletId;
+    type Data = ();
+
+    fn create(money: Money, _: Self::Data) -> Self {
+        Self(money)
+    }
+}
+
+#[derive(Debug, Clone, serde::Serialize, serde::Deserialize)]
+pub struct Debt {
+    money: Money,
+    actor_id: ActorId,
+}
+
+impl AsMut<Money> for Debt {
+    fn as_mut(&mut self) -> &mut Money {
+        &mut self.money
+    }
+}
+
+impl MoneyHost for Debt {
+    type Key = DebtId;
+    type Data = ActorId;
+
+    fn create(money: Money, data: Self::Data) -> Self {
+        Self {
+            money,
+            actor_id: data,
+        }
+    }
+}
+
+#[derive(Debug, Default, Clone, serde::Serialize, serde::Deserialize)]
+pub struct Snapshot {
+    pub wallets: MoneyMap<Wallet>,
+    /// Debts that I should pay
+    pub debts: MoneyMap<Debt>,
+    /// Debts that should be payed to me
+    pub loans: MoneyMap<Debt>,
+}
+
+#[derive(Debug, Clone, serde::Serialize, serde::Deserialize)]
+#[serde(tag = "type", rename_all = "snake_case")]
+pub enum WalletOperation {
+    Create {
+        wallet_id: WalletId,
+        currency_id: CurrencyId,
+    },
+    /// Does not handle transference before deletion
+    Delete {
+        wallet_id: WalletId,
+    },
+    Deposit {
+        wallet_id: WalletId,
+        amount: Amount,
+    },
+    Deduct {
+        wallet_id: WalletId,
+        amount: Amount,
+    },
+}
+
+#[derive(Debug, Clone, serde::Serialize, serde::Deserialize)]
+#[serde(tag = "type", rename_all = "snake_case")]
+pub enum DebtOperation {
+    Incur {
+        debt_id: DebtId,
+        currency_id: CurrencyId,
+        actor_id: ActorId,
+    },
+    /// Does not handle transference before deletion
+    Forget {
+        debt_id: DebtId,
+    },
+    Accumulate {
+        debt_id: DebtId,
+        amount: Amount,
+    },
+    Amortize {
+        debt_id: DebtId,
+        amount: Amount,
+    },
+}
+
+#[derive(Clone, Debug, serde::Serialize, serde::Deserialize)]
+#[serde(tag = "group", rename_all = "snake_case")]
+pub enum Operation {
+    Wallet(WalletOperation),
+    Loan(DebtOperation),
+    Debt(DebtOperation),
+}
+
+#[derive(Debug)]
+pub enum Error {
+    Wallet(MoneyError),
+    Debt(MoneyError),
+    Loan(MoneyError),
+}
+
+impl std::fmt::Display for Error {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        fn print_debt_err(e: &MoneyError, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+            match e {
+                MoneyError::NotFound => write!(f, "debt not found"),
+                MoneyError::CannotSub => write!(f, "debt amortize overflow"),
+                MoneyError::AlreadyExists => write!(f, "debt already exists"),
+            }
+        }
+
+        match self {
+            Error::Wallet(e) => match e {
+                MoneyError::NotFound => write!(f, "wallet not found"),
+                MoneyError::CannotSub => write!(f, "cannot deduct from wallet"),
+                MoneyError::AlreadyExists => write!(f, "wallet already exists"),
+            },
+            Error::Debt(e) => {
+                write!(f, "in ")?;
+                print_debt_err(e, f)
+            }
+            Error::Loan(e) => {
+                write!(f, "out ")?;
+                print_debt_err(e, f)
+            }
+        }
+    }
+}
+
+impl std::error::Error for Error {}
+
+impl Snapshot {
+    pub fn apply(&mut self, event: Operation) -> Result<(), Error> {
+        fn apply_debt_operation(
+            moneys: &mut MoneyMap<Debt>,
+            event: DebtOperation,
+        ) -> Result<(), MoneyError> {
+            match event {
+                DebtOperation::Incur {
+                    debt_id,
+                    currency_id,
+                    actor_id,
+                } => moneys.create(debt_id, currency_id, actor_id),
+                DebtOperation::Forget { debt_id } => moneys.remove(debt_id),
+                DebtOperation::Accumulate { debt_id, amount } => moneys.add(debt_id, amount),
+                DebtOperation::Amortize { debt_id, amount } => moneys.sub(debt_id, amount),
+            }
+        }
+
+        match event {
+            Operation::Wallet(operation) => {
+                let result = match operation {
+                    WalletOperation::Create {
+                        wallet_id,
+                        currency_id,
+                    } => self.wallets.create(wallet_id, currency_id, ()),
+                    WalletOperation::Deposit { wallet_id, amount } => {
+                        self.wallets.add(wallet_id, amount)
+                    }
+                    WalletOperation::Deduct { wallet_id, amount } => {
+                        self.wallets.sub(wallet_id, amount)
+                    }
+                    WalletOperation::Delete { wallet_id } => self.wallets.remove(wallet_id),
+                };
+
+                result.map_err(Error::Wallet)
+            }
+            Operation::Debt(operation) => {
+                apply_debt_operation(&mut self.debts, operation).map_err(Error::Debt)
+            }
+            Operation::Loan(operation) => {
+                apply_debt_operation(&mut self.loans, operation).map_err(Error::Loan)
+            }
+        }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn it_creates_debt() {
+        let mut snapshot = Snapshot::default();
+        let debt_id = DebtId::new();
+        let currency_id = CurrencyId::new();
+
+        let action = DebtOperation::Incur {
+            debt_id,
+            currency_id,
+            actor_id: ActorId::new(),
+        };
+
+        snapshot.apply(Operation::Debt(action)).unwrap();
+
+        assert_eq!(snapshot.debts.len(), 1);
+    }
+}
---
+++ monee/monee_core/src/snapshot/money.rs
@@ -0,0 +1,108 @@
+use crate::{Amount, CurrencyId};
+use std::{collections::HashMap, hash::Hash};
+
+#[derive(Debug, Clone, serde::Serialize, serde::Deserialize)]
+pub struct Money {
+    amount: Amount,
+    currency_id: CurrencyId,
+}
+
+#[derive(Debug, Clone, serde::Serialize, serde::Deserialize)]
+pub struct MoneyMap<M: MoneyHost>(HashMap<M::Key, M>);
+
+impl<M: MoneyHost> Default for MoneyMap<M> {
+    fn default() -> Self {
+        Self(HashMap::new())
+    }
+}
+
+pub trait MoneyHost: AsMut<Money> {
+    type Key: Hash + Eq + serde::Serialize + serde::de::DeserializeOwned;
+    type Data: serde::Serialize + serde::de::DeserializeOwned;
+
+    fn create(money: Money, data: Self::Data) -> Self;
+}
+
+impl<M: MoneyHost> MoneyMap<M> {
+    pub(crate) fn create(
+        &mut self,
+        key: M::Key,
+        currency_id: CurrencyId,
+        data: M::Data,
+    ) -> Result<(), MoneyError> {
+        if self.0.contains_key(&key) {
+            return Err(MoneyError::AlreadyExists);
+        }
+
+        self.0.insert(
+            key,
+            M::create(
+                Money {
+                    amount: Amount::default(),
+                    currency_id,
+                },
+                data,
+            ),
+        );
+
+        Ok(())
+    }
+
+    pub(crate) fn add(&mut self, key: M::Key, amount: Amount) -> Result<(), MoneyError> {
+        let money = self.0.get_mut(&key).ok_or(MoneyError::NotFound)?;
+        money.as_mut().amount += amount;
+
+        Ok(())
+    }
+
+    pub(crate) fn sub(&mut self, key: M::Key, amount: Amount) -> Result<(), MoneyError> {
+        let money = self.0.get_mut(&key).ok_or(MoneyError::NotFound)?;
+        let stored_amount = &mut money.as_mut().amount;
+        let result = stored_amount
+            .checked_sub(amount)
+            .ok_or(MoneyError::CannotSub)?;
+        *stored_amount = result;
+
+        Ok(())
+    }
+
+    pub(crate) fn remove(&mut self, key: M::Key) -> Result<(), MoneyError> {
+        match self.0.remove(&key) {
+            Some(_) => Ok(()),
+            None => Err(MoneyError::NotFound),
+        }
+    }
+
+    pub fn get(&self, k: &M::Key) -> Option<&M> {
+        self.0.get(k)
+    }
+
+    pub fn len(&self) -> usize {
+        self.0.len()
+    }
+
+    pub fn is_empty(&self) -> bool {
+        self.0.is_empty()
+    }
+
+    pub fn iter(&self) -> std::collections::hash_map::Iter<'_, M::Key, M> {
+        self.0.iter()
+    }
+}
+
+impl<M: MoneyHost> IntoIterator for MoneyMap<M> {
+    type Item = <HashMap<M::Key, M> as IntoIterator>::Item;
+    type IntoIter = <HashMap<M::Key, M> as IntoIterator>::IntoIter;
+
+    fn into_iter(self) -> Self::IntoIter {
+        self.0.into_iter()
+    }
+}
+
+#[derive(Debug)]
+pub enum MoneyError {
+    NotFound,
+    CannotSub,
+    AlreadyExists,
+}
+
