6ef0deaa74916efdddb1ab6a2396a36656f2e7a5
Jeshua Hinostroza <hinostrozajeshua@gmail.com>
2024-08-18 20:11:22 -0500

    refactor(lib): remove unused old code

--- monee/src/lib.rs
+++ monee/src/lib.rs
@@ -1,20 +1,13 @@
-pub mod error;
-pub mod log;
-pub mod ops;
-
-// pub mod actions;
-// pub mod procedures;
-
-pub mod snapshot_io;
-
-use std::{fs, path::PathBuf};
-
 pub mod date {
     pub type Datetime = chrono::DateTime<chrono::Utc>;
     pub use chrono::Utc as Timezone;
 }
 
-fn create_local_path() -> PathBuf {
+// TODO
+#[cfg(feature = "embedded")]
+fn create_local_path() -> std::path::PathBuf {
+    use std::{fs, path::PathBuf};
+
     let share_dir = std::env::var("XDG_DATA_HOME")
         .ok()
         .map(PathBuf::from)
