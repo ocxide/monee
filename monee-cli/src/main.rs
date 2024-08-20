18a225b43ae0c27b6cf552e0857ae49dc36a05c7
Jeshua Hinostroza <hinostrozajeshua@gmail.com>
2024-08-20 12:06:53 -0500

    feat(cli): can log infra errors

--- monee-cli/src/main.rs
+++ monee-cli/src/main.rs
@@ -1,4 +1,27 @@
+mod alias;
 mod date;
+
+mod error {
+    use monee::shared::{domain::context::AppContext, infrastructure::errors::InfrastructureError};
+
+    pub struct PanicError(InfrastructureError);
+
+    impl PanicError {
+        pub fn new(err: InfrastructureError) -> Self {
+            Self(err)
+        }
+
+        pub fn into_final_report(self, ctx: &AppContext) -> miette::Report {
+            let service = ctx.provide::<monee::shared::application::logging::LogService>();
+            service.error(self.0);
+
+            miette::diagnostic! {
+                "Unhandable error, logging error"
+            }
+            .into()
+        }
+    }
+}
 
 use alias::MaybeAlias;
 use clap::Parser;
@@ -28,102 +51,6 @@ enum WalletCommand {
     },
 }
 
-mod alias {
-    use std::{fmt::Display, str::FromStr};
-
-    use monee::shared::domain::context::AppContext;
-    use monee_core::CurrencyId;
-
-    pub trait AliasedId: Sized + FromStr + Clone {
-        type Alias: FromStr + Display + Clone;
-
-        async fn resolve(
-            ctx: &AppContext,
-            alias: Self::Alias,
-        ) -> Result<Option<Self>, monee::shared::infrastructure::errors::InfrastructureError>;
-    }
-
-    #[derive(Clone)]
-    pub enum MaybeAlias<I: AliasedId> {
-        Alias(I::Alias),
-        Id(I),
-    }
-
-    #[allow(private_bounds)]
-    impl<I> MaybeAlias<I>
-    where
-        I: AliasedId,
-    {
-        pub async fn resolve(self, ctx: &AppContext) -> Result<I, miette::Error> {
-            let alias = match self {
-                Self::Id(id) => return Ok(id),
-                Self::Alias(alias) => alias,
-            };
-
-            let alias_str = alias.to_string();
-            match I::resolve(ctx, alias).await {
-                Ok(Some(id)) => Ok(id),
-                Ok(None) => Err({
-                    miette::miette!(code = "NotFound", "Could not resolve alias `{}`", alias_str)
-                }),
-                Err(err) => Err({
-                    miette::miette!(
-                        code = "InfrastructureError",
-                        "Unknown error resolving alias: {}",
-                        err
-                    )
-                }),
-            }
-        }
-    }
-
-    #[derive(Debug, thiserror::Error)]
-    pub enum ParseError<I>
-    where
-        I: AliasedId,
-        I::Err: std::error::Error + Send + Sync + 'static,
-        <I::Alias as FromStr>::Err: std::error::Error + Send + Sync + 'static,
-    {
-        #[error(transparent)]
-        Alias(<I::Alias as FromStr>::Err),
-        #[error(transparent)]
-        Id(<I as FromStr>::Err),
-    }
-
-    impl<I> std::str::FromStr for MaybeAlias<I>
-    where
-        I: AliasedId,
-        I::Err: std::error::Error + Send + Sync + 'static,
-        <I::Alias as FromStr>::Err: std::error::Error + Send + Sync + 'static,
-    {
-        type Err = ParseError<I>;
-
-        fn from_str(s: &str) -> Result<Self, Self::Err> {
-            if let Some(id) = s.strip_prefix("id:") {
-                let id = I::from_str(id).map_err(ParseError::Id)?;
-                return Ok(Self::Id(id));
-            }
-
-            let alias = I::Alias::from_str(s).map_err(ParseError::Alias)?;
-            Ok(Self::Alias(alias))
-        }
-    }
-
-    impl AliasedId for CurrencyId {
-        type Alias = String;
-
-        async fn resolve(
-            ctx: &AppContext,
-            alias: Self::Alias,
-        ) -> Result<Option<Self>, monee::shared::infrastructure::errors::InfrastructureError>
-        {
-            let service = ctx
-                .provide::<monee::backoffice::currencies::application::code_resolve::CodeResolve>();
-            service.run(&alias).await
-        }
-    }
-}
-
 #[tokio::main]
 async fn main() -> miette::Result<()> {
     let (ctx, main_task) = monee::shared::domain::context::setup()
@@ -150,10 +77,12 @@ async fn main() -> miette::Result<()> {
                     currency_id,
                 };
 
-                service.run(wallet).await.expect("To create wallet");
+                service
+                    .run(wallet)
+                    .await
+                    .map_err(|e| error::PanicError::new(e).into_final_report(&ctx))
+                    .map(|_| ())
             }
         },
     }
-
-    Ok(())
 }
