b1aa9801c39ec69a875972112ddf1d072d7f9179
Jeshua Hinostroza <hinostrozajeshua@gmail.com>
2024-10-13 09:38:06 -0500

    fix(db): make wallet names unique

--- monee/src/shared/infrastructure/database.rs
+++ monee/src/shared/infrastructure/database.rs
@@ -24,7 +24,7 @@ async fn init(connection: &Connection) -> Result<()> {
         .query("DEFINE FIELD name ON wallet TYPE option<string>")
         .query("DEFINE FIELD description ON wallet TYPE string")
         .query("DEFINE FIELD currency_id ON wallet TYPE record<currency>")
-        .query("DEFINE INDEX wallet_name ON wallet_metadata FIELDS name UNIQUE")
+        .query("DEFINE INDEX wallet_name ON wallet FIELDS name UNIQUE")
         .await?
         .check()?;
 
