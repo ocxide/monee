649ef8d481f4651bd17443aae6b9afd7d986c17e
Jeshua Hinostroza <hinostrozajeshua@gmail.com>
2024-12-08 22:00:06 -0500

    refactor(lib): prelude cream context

--- monee/src/lib.rs
+++ monee/src/lib.rs
@@ -31,4 +31,6 @@ pub mod prelude {
     pub use crate::shared::domain::context::AppContext;
     pub use crate::shared::infrastructure::errors::AppError;
     pub use crate::shared::infrastructure::errors::InfrastructureError;
+
+    pub use cream::context::Context;
 }
