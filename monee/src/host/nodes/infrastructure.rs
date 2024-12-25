51193c6ffbd2cac06e286501d80a2a9692c37753
Jeshua Hinostroza <hinostrozajeshua@gmail.com>
2024-12-25 13:07:16 -0500

    fix(lib): check exists of node id

--- monee/src/host/nodes/infrastructure.rs
+++ monee/src/host/nodes/infrastructure.rs
@@ -1,7 +1,10 @@
 pub mod repository {
     use monee_types::apps::{app_id::AppId, app_manifest::AppManifest};
 
-    use crate::{host::nodes::domain::repository::Repository, shared::domain::context::DbContext};
+    use crate::{
+        host::nodes::domain::repository::Repository,
+        shared::{domain::context::DbContext, infrastructure::database::EntityKey},
+    };
     pub use crate::{iprelude::*, prelude::*};
 
     #[derive(FromContext)]
@@ -23,13 +26,48 @@ pub mod repository {
         async fn exists(&self, id: AppId) -> Result<bool, InfrastructureError> {
             let mut response = self
                 .0
-                .query("SELECT id FROM ONLY node WHERE id = $id LIMIT 1")
+                .query("SELECT id FROM ONLY type::thing('node', $id) LIMIT 1")
                 .bind(("id", id))
                 .await?
                 .check()?;
 
-            let entity: Option<()> = response.take(0)?;
+            let entity: Option<EntityKey<AppId>> = response.take("id")?;
             Ok(entity.is_some())
+        }
+    }
+
+    #[cfg(test)]
+    mod tests {
+        #![allow(unused)]
+        use crate::shared::infrastructure::database::connect;
+
+        use super::*;
+
+        #[cfg(feature = "db_test")]
+        #[tokio::test]
+        async fn checks_exists() {
+            let db = connect().await.unwrap();
+            let ctx = DbContext::new(db);
+
+            let repo: SurrealRepository = ctx.provide();
+            let id = AppId::default();
+            repo.save(id, AppManifest { name: None })
+                .await
+                .expect("To save");
+
+            assert!(repo.exists(id).await.expect("To check"));
+        }
+
+        #[cfg(feature = "db_test")]
+        #[tokio::test]
+        async fn fails_doesnt_exist() {
+            let db = connect().await.unwrap();
+            let ctx = DbContext::new(db);
+
+            let repo: SurrealRepository = ctx.provide();
+            let id = AppId::default();
+
+            assert!(!repo.exists(id).await.expect("To check"));
         }
     }
 }
